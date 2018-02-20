9c49d2d9d5aa2643a2176636e33c924590a3b71c
GauthamArunachalam <gauthamarunachalam1998@gmail.com>
2018-02-21 05:13:53 +0530

    first part

---
+++ Learn.AI/New Unity Project (2)/Assets/Trail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+	Rigidbody rb;
+	public float speed = 1000.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float Hmove = Input.GetAxis ("Horizontal");
+		float Vmove = Input.GetAxis ("Vertical");
+
+		Vector3 moveball = new Vector3 (Hmove, 0.0f, Vmove);
+		rb.AddForce (moveball * speed * Time.deltaTime);
+			
+	}
+}
